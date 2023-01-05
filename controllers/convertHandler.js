5150b39ec1383ce5de3fecbd2e4755781b67626f
Crepaldi93 <96325460+Crepaldi93@users.noreply.github.com>
2023-01-05 10:22:39 -0300

    feat: getUnit funcion in ConvertHandler

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -4,23 +4,47 @@ function ConvertHandler() {
     let result;
 
     // Define regular expression to extract integer, float or fraction numbers
-    let regEx = /[\d\.\/]+/
+    let regEx = /[\d\.\/]+/;
 
     // Extract number from string
-    let numberString = string.match(regEx)
+    let numberString = input.match(regEx);
 
     // Check if number is null, integer, decimal or fraction and return result accordingly
-    if ()
+    if (!numberString) {
+      result = 1
+    } else if (numberString.includes("/")) {
+      let divisionNumbers = numberString.split("/");
 
-
-
-
+      if (divisionNumbers.length > 2 || divisionNumbers[1] == 0) {
+        result = "invalid number";
+      } else {
+        result = divisionNumbers[0] / divisionNumbers[1];
+      }
+    } else {
+      result = numberString * 1
+    }
     
     return result;
   };
   
   this.getUnit = function(input) {
     let result;
+
+    // Define regular expression to extract integer, float or fraction numbers
+    let regEx = /[\d\.\/]+/;
+
+    // Get string without the characters defined in the regular expression
+    let myUnit = input.replace(regEx, "").toLowerCase()
+
+    // Create array of units supported by the app
+    let unitsArray = ["gal", "l", "mi", "km", "lbs", "kg"]
+
+    // Return unit string if valid or error message if invalid
+    if (unitsArray.includes(myUnit)) {
+      result = myUnit
+    } else {
+      result = "invalid unit"
+    }
     
     return result;
   };
